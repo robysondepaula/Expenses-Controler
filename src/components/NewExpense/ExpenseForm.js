b22347480b593133b4a447c4f024b88a6a1ad085
robysondepaula <robysonwillyandepaula@gmail.com>
2022-05-31 14:38:47 -0400

    Set up data from child to parent ExpenseForm > NewExpense > App.js

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [amountEntered, setEnteredAmount] = useState('');
     const [dateEntered, setEnteredDate] = useState('');
@@ -27,7 +27,10 @@ const ExpenseForm = () => {
           date:new Date(dateEntered)
       }
 
-      console.log(expenseData);
+      props.onSaveExpenseData(expenseData);
+      setEnteredTitle("");
+      setEnteredAmount("");
+      setEnteredDate("");
   };
 
   return (
@@ -35,15 +38,15 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
-          <label>amount</label>
-          <input type="value" min="0.01" step="0.01" onChange={amountChangeHandler} />
+          <label>Amount</label>
+          <input type="value" min="0.01" step="0.01" value={amountEntered} onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2020-01-01" max="2023-12-31" onChange={dateChangeHandler}/>
+          <input type="date" min="2020-01-01" max="2023-12-31" value={dateEntered} onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className="new-expense__actions">
