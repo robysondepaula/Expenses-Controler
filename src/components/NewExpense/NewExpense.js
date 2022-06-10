e475da6cb9f7bd683612ed18b7ad13a6ca50fc5c
robysondepaula <robysonwillyandepaula@gmail.com>
2022-06-10 17:29:10 -0400

    Adding the cancel but and seting the defalut button to user

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,41 @@
-import './NewExpense.css';
-import ExpenseForm from './ExpenseForm';
+import React, { useState } from "react";
+
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
-    const saveExpenseHandler = (enteredExpenseData) => {
-      const expenseData ={
-          ...enteredExpenseData,
-          id: Math.random().toString()
-      }; 
-      props.onAddExpense(expenseData);
+  const [isEdditing, setIsEdditing] = useState(false);
+
+  const saveExpenseHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
     };
+    props.onAddExpense(expenseData);
+    setIsEdditing(false);
+  };
 
-    return <div className="new-expense">
-       <ExpenseForm onSaveExpenseData = {saveExpenseHandler} />
+  const startEdditingHandler = () => {
+    setIsEdditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEdditing(false);
+  };
+
+  return (
+    <div className="new-expense">
+      {!isEdditing && (
+        <button onClick={startEdditingHandler}>Add New Expense</button>
+      )}
+      {isEdditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
+  );
 };
-
 
 export default NewExpense;
